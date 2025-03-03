052c257f5e5d7fad20a3960efa8f2ac4d0f2ded3
ivakin.ma <ivakin.ma@samos.dozen.mephi.ru>
2025-03-03 11:50:28 +0300

    extended version lab1

--- 1/binmain.c
+++ 1/binmain.c
@@ -4,15 +4,15 @@
 #include"bin.h"
 int main(){
 		long unsigned int pswd = 17777852590576505418UL;
-        Mat *mat = calloc(1, sizeof(Mat));
-        if(inputmat(mat)==CLOSE){
-                return 0;
-        }
+        // Mat *mat = calloc(1, sizeof(Mat));
+        // if(inputmat(mat)==CLOSE){
+        //         return 0;
+        // }
         char *name;
         printf("Введите название файла: ");
         scanf("%s", name);
-        FILE *f = fopen(name, "w");
-        creator(f, pswd, mat);
-        freemat(mat);
-        printf("%zu %zu", sizeof(Mat), sizeof(Line));
+        FILE *f = fopen(name, "rb");
+        binprocess(f, pswd);
+        // creator(f, pswd, mat);
+        // freemat(mat);
 }
